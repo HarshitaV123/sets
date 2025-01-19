6750c8e3243a74b6c404a3c66d1630a6f5b2acd6
HarshitaV123 <100452399+HarshitaV123@users.noreply.github.com>
2025-01-19 08:53:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#creating sets
+A={1,2,3,4,5,6}
+B={5,6,7,8,4,2}
+#union of sets
+print(A|B)
+print(A.union(B))
+#intersection of sets
+print(A&B)
+print(A.intersection(B))
+#difference of sets
+print(A-B)
+#conversion of list to set
+my_list=[1,2,3,4]
+my_set = set(my_list)
+print(my_set)
