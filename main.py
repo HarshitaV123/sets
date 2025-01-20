f8cb08a0e831eee7fdc0a5cafd77c12ceb89b7fd
HarshitaV123 <100452399+HarshitaV123@users.noreply.github.com>
2025-01-20 13:20:09 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ print(A-B)
 my_list=[1,2,3,4]
 my_set = set(my_list)
 print(my_set)
+#symmetric difference
+print(A.symmetric_difference(B))
+print(A^B)
+#adding element to empty set
+numset=set([])
+numset.add(6)
+numset.add(8)
+numset.add(7)
+numset.add(3)
+print(numset)
+#removing an element from the set
+numset.remove(6)
+numset.discard(8)
+print(numset)
